0769ad2a03225eab4b64e9655eaa55e08a6a4c9c
Shy1011 <150208066+Shy1011@users.noreply.github.com>
2024-01-26 18:26:32 +0800

    done

--- pythonProject/excel_1.py
+++ pythonProject/excel_1.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
 import xlwings as xw
-# wb = xw.Book()  # 这将创建一个新的工作簿
-wb = xw.Book('FileName.xlsx')  # 连接到当前工作目录中的现有文件
-# wb = xw.Book(r'C:\path\to\file.xlsx')  # 在Windows上：使用原始字符串来转义反斜杠
+app = xw.App(visible = True, add_book = False)
+workbook = app.books.add()
 
-sht = wb.sheets['Sheet1']
-sht.range('A1').value = [['Foo 1', 'Foo 2', 'Foo 3'], [10.0, 20.0, 30.0]] #写入数据
-print(sht.range('A1').value)
-print(sht.range('A1').expand().value) # 读取数据
+workbook.save('example.xlsx')
+workbook.close() # 关闭工作簿
+app.quit() # 退出Excel程序
